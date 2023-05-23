b8cf58baeb43d6f2837188cb8132c496d9a39c3d
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2023-05-23 18:28:25 +0200

    Add ubi8

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM registry.fedoraproject.org/fedora:38
+FROM registry.access.redhat.com/ubi8/ubi:latest
 ADD * /setup/
 RUN sh -eux /setup/setup.sh
 CMD /bin/bash
--- setup.sh
+++ setup.sh
@@ -1,20 +1,7 @@
-# before anything else: re-enable install of manpages
-sed -ie 's/\<nodocs\>//' /etc/dnf/dnf.conf
-# reinstall system packages which should have had manpages
-dnf reinstall -y $(rpm -qa --queryformat '%{NAME}[:%{FILENAMES}]\n' |
-                   grep ':/usr/share/man/' | cut -f1 -d:)
-
-# https://github.com/containers/toolbox/pull/640
-cat > /usr/lib/rpm/macros.d/macros.toolbox <<EOF
-%_netsharedpath /dev:/media:/mnt:/proc:/sys:/tmp:/var/lib/flatpak:/var/lib/libvirt
-EOF
-
 dnf update -y
-dnf install -y $(</setup/install) dnf-plugins-core
-dnf build-dep -y $(</setup/install-build-dep) filesystem
-dnf debuginfo-install -y $(</setup/install-debuginfo) filesystem
+dnf install -y dnf-plugins-core python3
+dnf install -y $(</setup/install)
+python3 -m pip install $(</setup/install-pypi)
+
 dnf clean all
-
-pip install diffoscope
-
 rm -r /setup
